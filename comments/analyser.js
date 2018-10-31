06202c0c4d9e4240b1b0ed43cb524696e70259f3
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2018-10-31 00:00:41 -0300

    upload thing

---
+++ comments/analyser.js
@@ -0,0 +1,105 @@
+var colours = ['red', '#131369', '#136913', '#691369', '#696913'];
+var posts = {};
+var ready = false;
+var ready_count = 0;
+var ready_total = 0;
+var loading = document.querySelector('span');
+
+function getUrl(user, after) {
+	var base = 'https://www.reddit.com/user/';
+	base += user + '.json?limit=100';
+	if (after) base += ('&after=' + after);
+	return base;
+}
+
+function loadInput() {
+	var input = document.querySelector('input');
+	if (!ready || input.value === '') {
+		return;
+	}
+
+	loading.style.display = 'inline';
+	load(input.value.split(',').map(i => i.trim()).slice(0,5));
+}
+
+function load(users) {
+	ready = false;
+	ready_count = 0;
+	ready_total = users.length;
+	window.config.data.datasets = [];
+	for (var user of users) {
+		loader(user);
+	}
+}
+
+function loader(user, after) {
+	if (!posts[user]) posts[user] = [];
+
+	after = after || '';
+	var url = getUrl(user, after);
+
+	console.log('Loading:', url)
+	axios.get(url).then(function(response) {
+		var d = response.data.data;
+		posts[user] = posts[user].concat(
+			d.children.filter(
+				u => u.kind === 't1' && u.data.subreddit == 'chile'
+			).map(
+				u => u.data
+			)
+		);
+
+		if (d.after !== null) {
+			loader(user, d.after);
+		} else {
+			process(user, posts[user]);
+		}
+	}).catch(function(e) {
+		console.exception(e);
+		ready_total += 1;
+	});
+}
+
+function process(user, ps) {
+	var data = ps.map(
+		i => moment(new Date(parseInt(i.created_utc) * 1000)).format('YYYY-MM-DD')
+	).reduce((p, i) => {
+		if (Object.keys(p).indexOf(i) == -1) {
+			p[i] = 1;
+		} else {
+			p[i]++;
+		}
+
+		return p;
+	}, {});
+
+	data = Object.keys(data).map(k => { return { x: moment(k, 'YYYY-MM-DD').toDate(), y: data[k] }; });
+
+	var l = window.config.data.datasets.length;
+	var colour = l > colours.length ? randColour() : colours[window.config.data.datasets.length];
+	window.config.data.datasets.push({
+		label: user,
+		backgroundColor: colour,
+		borderColor: colour,
+		data: data,
+		fill: false
+	});
+
+	ready_count += 1;
+	if (ready_count == ready_total) {
+		window.lineChart.update();
+		ready = true;
+		loading.style.display = 'none';
+	}
+}
+
+function randColour() {
+	var r = Math.floor(Math.random() * 255);
+	var g = Math.floor(Math.random() * 255);
+	var b = Math.floor(Math.random() * 255);
+	return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+window.addEventListener('load', function() {
+	ready = true;
+});
---
+++ comments/chart-config.js
@@ -0,0 +1,92 @@
+var rand = function() {
+	return Math.round(Math.random() * 100);
+};
+
+window.config = {
+	type: 'line',
+	data: {
+		datasets: []
+	},
+	options: {
+		responsive: true,
+		title: {
+			display: true,
+			text: '/r/Chile comments timeline'
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		scales: {
+			xAxes: [{
+				type: 'time',
+				time: {
+					parser: 'YYYY-MM-DD',
+					round: 'day',
+					min: moment().subtract(1, 'years'),
+					fillGapsWithZero: true
+				},
+				scaleLabel: {
+					display: true,
+					labelString: 'Date'
+				},
+				minUnit: 'day'
+			}],
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'comments'
+				}
+			}]
+		}
+	}
+};
+
+window.addEventListener('load', function() {
+	var fillScatteredTimeScaleDataPlugin = {
+		beforeUpdate: function(c) {
+			var timeAxis = c.options.scales.xAxes[0].time;
+			if (!timeAxis || !timeAxis.fillGapsWithZero) return;
+
+			for (var i=0;i<c.data.datasets.length;i++) {
+				var set = c.data.datasets[i];
+				var min, max, hash = {};
+
+				for (var j=0;j<set.data.length;j++){
+					var val = moment(set.data[j].x, timeAxis.parser);
+					if (!min || min.diff(val)>0)
+						min = val;
+					if (!max || max.diff(val)<0)
+						max = val;
+					hash[val.format(timeAxis.parser)] = 1;
+				}
+
+				var curr = min.clone();
+
+				for (var val = 0; val < max.diff(min, 'days'); val++){
+					curr.add(1, 'd');
+
+					var d = curr.format(timeAxis.parser);
+
+					if (Object.keys(hash).indexOf(d) === -1) {
+						set.data.push({x: curr.toDate(), y: 0});
+					}
+				}
+				
+				set.data.sort(function(a,b){
+					return a.x < b.x?-1:1;
+				});
+			}
+		}
+	}
+
+	Chart.pluginService.register(fillScatteredTimeScaleDataPlugin);
+
+	window.lineChart = new Chart(
+		document.querySelector('canvas'), window.config
+	);
+});
